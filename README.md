160b752ff25539bf5ca507d8b5f4658fdc01e649
lxyoucan <lxyoucan@users.noreply.github.com>
2021-08-02 14:38:57 +0800

    增加CentOS7安装oh my zsh的脚本

--- README.md
+++ README.md
@@ -1,44 +1,31 @@
-
-# Xfce桌面一键VNC脚本
+# CentOS7专用脚本
+因为我平时接触的大部分机器都是CentOS7,这里的脚本都是针对CentOS7开发的。虽然个人比较喜欢新的软件，喜欢archlinux这样的发行版本，但是服务器上使用CentOS的还是要更多一些的。
+## Xfce桌面一键VNC脚本
 - Xfce桌面
 - fcitx输入法框架
 - 搜狗输入法
 - 五笔输入法
 
-
-使用root用户执行以下命令，即可简单快捷的完成以上软件的安装。
 ```bash
 sh -c "$(curl -fsSL https://gitee.com/lxyoucan/tools/raw/master/centos7/XfceVnc.sh)"
 ```
-
 注意：
 1. **脚本需要root权限执行**
-2. **脚本仅支持CentOS7**
-
-# Openbox一键VNC脚本
+## Openbox一键VNC脚本
 - Openbox
 - fcitx输入法框架
 - 搜狗输入法
 - 五笔输入法
 
-
-使用root用户执行以下命令，即可简单快捷的完成以上软件的安装。
 ```bash
 sh -c "$(curl -fsSL https://gitee.com/lxyoucan/tools/raw/master/centos7/OpenboxVnc.sh)"
 ```
 
 注意：
 1. **脚本需要root权限执行**
-2. **脚本仅支持CentOS7**
 
-## 说明
+### 说明
 脚本绿色无后门，可以重复执行。目前只有安装脚本，如果需要卸载，请参考安装脚本。手动执行yum remove .....。
-
-- VNC服务的端口是：5901
-- 开启VNC服务：`systemctl start vncserver@:1`
-- 开启VNC服务：`systemctl stop vncserver@:1`
-- 开机启动VNC服务：`systemctl enable vncserver@:1`
-- 禁用开机启动VNC服务：`systemctl disable vncserver@:1`
 
 客户端连接示例：
 随便找一个vnc客户端，连接地址： ip(或者域名)+:5901
@@ -48,16 +35,7 @@ sh -c "$(curl -fsSL https://gitee.com/lxyoucan/tools/raw/master/centos7/OpenboxV
 ```
 ![172.16.184.5:5901](https://img-blog.csdnimg.cn/d698b45ec622433980912f3b0ad0cad5.png)
 
-
-# Gnome一键VNC脚本
-因为Gnome在vnc中使用fcitx输入法，部分软件无法正常输入中文的，所以Gnome使用自带的ibus输入法就行。
-Gnome 使用ibus输入法很简单，参考
-[https://blog.csdn.net/lxyoucan/article/details/113179208](https://blog.csdn.net/lxyoucan/article/details/113179208)
-手动安装即可。
-
-如果有需要一键安装脚本，评论留言我会考虑开发一下。
-
-# 一键搭建nvim + react开发环境
+## 一键搭建nvim + react开发环境
 - python3
 - node.js v14
 - nvim-coc等等相关vim插件
@@ -65,13 +43,23 @@ Gnome 使用ibus输入法很简单，参考
 ```bash
 sh -c "$(curl -fsSL https://gitee.com/lxyoucan/tools/raw/master/centos7/nvim/nvimIDE.sh)"
 ```
-# 国内加速oh my zsh一键安装脚本
+
+## oh my zsh 一键安装
+```bash
+sh -c "$(curl -fsSL https://gitee.com/lxyoucan/tools/raw/master/centos7/ohmyzsh.sh)"
+```
+注意：
+1. **脚本需要root权限执行**
+
+# 通用脚本
+理论上支持大部分linux发行版本或者macOS
+## 国内加速oh my zsh一键安装脚本
 官方原版本，只是把github库换成了CSDN的镜像。国内访问比较快。
 ```bash
 sh -c "$(curl -fsSL https://gitee.com/lxyoucan/tools/raw/master/common/ohmyzshinstall.sh)"
 ```
 
-# 国内加速oh my zsh ITKEY个性化配置版
+## 国内加速oh my zsh ITKEY个性化配置版
 集成如下插件
 - 语法高亮插件
 - 自动补全插件
---
+++ centos7/ohmyzsh.sh
@@ -0,0 +1,134 @@
+#oh my zsh插件，ITKEY个性化配置版本,CentOS7且需root权限。
+#国内网络加速
+#lxyoucan@163.com
+
+# 字符串染色程序
+if [[ -t 1 ]]; then
+  tty_escape() { printf "\033[%sm" "$1"; }
+else
+  tty_escape() { :; }
+fi
+tty_universal() { tty_escape "0;$1"; } #正常显示
+tty_mkbold() { tty_escape "1;$1"; }    #设置高亮
+tty_underline="$(tty_escape "4;39")"   #下划线
+tty_blue="$(tty_universal 34)"         #蓝色
+tty_red="$(tty_universal 31)"          #红色
+tty_green="$(tty_universal 32)"        #绿色
+tty_yellow="$(tty_universal 33)"       #黄色
+tty_bold="$(tty_universal 39)"         #加黑
+tty_cyan="$(tty_universal 36)"         #青色
+tty_reset="$(tty_escape 0)"            #去除颜色
+
+#获取硬件信息
+UNAME_MACHINE="$(uname -m)"
+if [[ "$UNAME_MACHINE" != "x86_64" ]]; then
+  echo "${tty_red}安装程序只支持x86_64环境下运行${tty_reset}"
+fi
+
+# 判断是Linux还是Mac os
+OS="$(uname)"
+if [[ "$OS" != "Linux" ]]; then
+  echo "${tty_red}安装程序只支持linux环境下运行${tty_reset}"
+fi
+
+MY_WHO_AM_I=$(whoami)
+#系统版本
+MY_REDHAT_RELEASE=$(cat /etc/redhat-release)
+MY_REDHAT_RELEASE=${MY_REDHAT_RELEASE:0:22}
+if ! [[ "$MY_REDHAT_RELEASE" == 'CentOS Linux release 7' ]]; then
+  echo "${tty_red}友情提醒：脚本只适用于CentOS Linux release 7，程序退出${tty_reset}"
+  exit
+fi
+
+#判断是否是root用户，如果不是则退出脚本
+if ! [[ "$MY_WHO_AM_I" =~ ^root$ ]]; then
+  echo "${tty_red}友情提醒：当前用户${MY_WHO_AM_I}不是root用户，权限不足，程序退出${tty_reset}"
+  exit
+fi
+
+echo -n "${tty_green}
+正在安装oh my zsh插件，ITKEY个性化配置版本...
+安装插件需要以下条件：
+- 脚本只在CentOS7下测试运行，不支持其他平台
+- 脚本需要root权限执行
+
+${tty_blue}
+
+1、继续安装    2、退出安装
+"
+# 原本这个变量是由用户输入的，为了减少用户输入操作，暂时写死了
+read -r MY_DOWN_NUM
+echo "${tty_reset}"
+case $MY_DOWN_NUM in
+"1")
+
+  #-----------判断是否已经安装了zsh------------
+  #ZSH版本
+  MY_ZSH=$(zsh --version)
+  MY_ZSH=${MY_ZSH:0:3}
+  if ! [[ "$MY_ZSH" == 'zsh' ]]; then
+    #git不存在则进行安装
+    echo "${tty_green}=======安装zsh=======${tty_reset}"
+    yum -y install zsh
+  fi
+  #-----------创建用户------------
+  echo "${tty_blue}请输入用户名(非root普通用户): "
+  read -r MY_USER
+  echo "${tty_reset}"
+  echo "${tty_green}正在创建用户${MY_USER},如果用户已经存在可忽略${tty_reset}"
+  adduser -s /bin/zsh "${MY_USER}"
+  usermod --shell /bin/zsh "${MY_USER}"
+  #-----------判断是否已经安装了git------------
+  #系统版本
+  MY_GIT=$(git --version)
+  MY_GIT=${MY_GIT:0:3}
+  if ! [[ "$MY_GIT" == 'git' ]]; then
+    #zsh不存在则进行安装
+    echo "${tty_green}=======安装git=======${tty_reset}"
+    yum -y install git
+  fi
+
+  #备份原文件
+  #备份原文件
+  echo "${tty_green}尝试备份~/.zshrc|~/.oh-my-zsh${tty_reset}"
+  echo "${tty_green}文件备份在：/home/${MY_USER}/.zshrc_bak_$(date +%Y%m%d"_"%H_%M_%S)${tty_reset}"
+  echo "${tty_green}文件备份在：/home/${MY_USER}/.oh-my-zsh_bak_$(date +%Y%m%d"_"%H_%M_%S)${tty_reset}"
+  mv "/home/${MY_USER}/.zshrc" "/home/${MY_USER}/.zshrc_bak_$(date +%Y%m%d"_"%H_%M_%S)"
+  mv "/home/${MY_USER}/.oh-my-zsh" "/home/${MY_USER}/.oh-my-zsh_bak_$(date +%Y%m%d"_"%H_%M_%S)"
+  echo "${tty_green}正在clone oh my zsh${tty_reset}"
+  git clone https://codechina.csdn.net/mirrors/ohmyzsh/ohmyzsh.git "/home/${MY_USER}/.oh-my-zsh"
+  echo "${tty_green}下载oh my zsh配置文件${tty_reset}"
+  curl -fLo "/home/${MY_USER}/.zshrc.oh-my-zsh" https://gitee.com/lxyoucan/tools/raw/master/common/.zshrc.oh-my-zsh
+  echo "${tty_green}下载oh my zsh 语法高亮插件${tty_reset}"
+  git clone https://codechina.csdn.net/mirrors/zsh-users/zsh-syntax-highlighting.git "/home/${MY_USER}/.oh-my-zsh/plugins/zsh-syntax-highlighting"
+  echo "${tty_green}下载oh my zsh 自动补全插件${tty_reset}"
+  git clone https://codechina.csdn.net/mirrors/zsh-users/zsh-autosuggestions.git "/home/${MY_USER}/.oh-my-zsh/plugins/zsh-autosuggestions"
+
+  echo "#加载oh my zsh插件" >"/home/${MY_USER}/.zshrc"
+  echo "source ~/.zshrc.oh-my-zsh" >>"/home/${MY_USER}/.zshrc"
+
+  echo "${tty_cyan}-------------------------使用帮助-------------------------${tty_reset}"
+  echo "${tty_cyan}已经成功安装在${tty_blue} /home/${MY_USER}/.oh-my-zsh${tty_reset}"
+  echo "${tty_cyan}oh my zsh配置文件在${tty_blue} /home/${MY_USER}/.zshrc.oh-my-zsh${tty_reset}"
+  echo "${tty_cyan}zsh配置文件在${tty_blue}/home/${MY_USER}/.zshrc${tty_reset}"
+  echo "${tty_cyan}
+以下方法可以看到效果：
+- ${MY_USER}重新登录shell 或者
+- root用户执行：
+${tty_blue}su ${MY_USER}${tty_cyan}
+- 或者${MY_USER}用户登录执行下面命令立即生效：
+${tty_blue}source ~/.zshrc${tty_cyan}
+备份的文件请自行决定是否删除。
+${tty_reset}"
+
+  echo "${tty_cyan}-------------------------使用帮助-------------------------${tty_reset}"
+  echo "${tty_green}oh my zsh 已经安装完成,祝您身体健康，万事如意！${tty_reset}"
+  ;;
+
+\
+  "2")
+  echo "
+    你选择了2、取消安装
+    "
+  ;;
+esac
